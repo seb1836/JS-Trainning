b92848e4cc10be016a24a4b89c08be1cbe1b549c
seb1836 <seb111.penna@gmail.com>
2019-05-16 18:34:25 +0200

    Object Exercise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import "./styles.css";
 
+const myObject = {};
+
+const createObj = () => {
+  myObject["character"] = ["John", "Roger", "TheRandomGuy"];
+  myObject["room"] = ["kitchen", "bedroom", "livingroom"];
+  myObject["weapon"] = ["hammer", "knife", "gun"];
+
+  return console.log(myObject);
+};
+
+createObj();
+
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
