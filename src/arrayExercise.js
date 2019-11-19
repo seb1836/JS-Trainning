24522d95ff11eeed5e08c1a36cf90399f5b6338a
seb1836 <seb111.penna@gmail.com>
2019-11-19 17:06:54 +0100

    added exercices 2 and 3

--- src/arrayExercise.js
+++ src/arrayExercise.js
@@ -1,15 +1,26 @@
-const simpleArray = [1, 2, 3];
+const simpleArray = [1, 2, 3, 4, 5];
 const nestedArray = [1, 2, 3, [4]];
+const emptyArray = [];
 const arrayCloner = array => {
   const clonedArray = array.map(entry => {
     return entry;
   });
   return clonedArray;
 };
-
+const arrayExtractor = (array, index) => {
+  if (index === 0 || index === undefined) {
+    return array.shift();
+  } else if (array.length === 0 || index < 0) {
+    return [];
+  }
+  const extractedArray = index > array.length ? array : array.slice(0, index);
+  console.log(extractedArray);
+  return extractedArray;
+};
 const arrayExercises = () => {
   console.log(arrayCloner(simpleArray), "simple");
   console.log(arrayCloner(nestedArray), "nested");
+  console.log(arrayExtractor(simpleArray, 3));
 };
 
 export default arrayExercises;
