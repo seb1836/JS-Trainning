7b915f36bbf8699bc2f47db191d8a35b913670ae
seb1836 <seb111.penna@gmail.com>
2019-12-09 17:47:46 +0100

    added exercise number 10

--- src/arrayExercise.js
+++ src/arrayExercise.js
@@ -2,6 +2,13 @@ const simpleArray = [1, 2, 3, 4, 5];
 const nestedArray = [1, 2, 3, [4]];
 const myString2 = "fsfsad";
 const myMap = Array.prototype.map;
+const nestedArray2 = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7]
+];
 
 const arrayCloner = array => {
   const clonedArray = array.map(entry => {
@@ -20,10 +27,20 @@ const arrayExtractor = (array, index) => {
   return extractedArray;
 };
 
+const displayElementOfNestedArray = array => {
+  for (let i = 0; i < array.length; i++) {
+    console.log("row" + i);
+    for (let j = 0; j < array[i].length; j++) {
+      console.log(array[i][j]);
+    }
+  }
+};
+
 const arrayExercises = () => {
   console.log(arrayCloner(simpleArray), "simple");
   console.log(arrayCloner(nestedArray), "nested");
   console.log(arrayExtractor(simpleArray, 3));
+  console.log(displayElementOfNestedArray(nestedArray2), "display nested");
 };
 
 export default arrayExercises;
